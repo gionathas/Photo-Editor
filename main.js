b777d1fc8c1187652c3939a2c9e2308b0c0b55d1
Gtx96 <gionathas@gmailc.om>
2018-07-03 16:48:12 +0200

    Added panel animation, and upload button

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
 $(document).ready(() => {
     
-    let imageUploaded = false;
+    const fadeTime = 300;
 
     const canvas = $("canvas");
     //const ctx = canvas.getContext('2d');
 
-    let activeEditor = $(".settings");
+    let imageUploaded = false;
 
-    $("#editBtn").click(() =>{
-        activeEditor.fadeOut(400,() =>{
-            $(".basic-edits").fadeIn();
-            //set active on icon bar
-            //update active editor
-        });
+    // $("#editBtn").click(($(this)) => {console.log("hello");    $(this).addClass("active")});
+
+    let activeEditor = $(".settings");
+    let activeIconBar = $("#settingsBtn");
+
+    //panel and icon bar animation
+    let panelAnimation = function(newEditor,newIconBar){
         
-    })
+        //hide old editor
+        activeEditor.fadeOut(fadeTime,() => {
+            //when animation finished...
+
+            //show new editor
+            newEditor.fadeIn();
+
+            //update active icon bar
+            activeIconBar.removeClass("active");
+            newIconBar.addClass("active");
+
+            //update the active editor and the active icon bar
+            activeEditor = newEditor;
+            activeIconBar = newIconBar;
+        });
+    }
+
+    // Assign all panel animation to icon bar element
+    $("#editBtn").click(function(){panelAnimation($(".basic-edits"),$(this))});
+    $("#settingsBtn").click(function(){panelAnimation($(".settings"),$(this))});
+    // $("#editBtn").click(() => panelAnimation($(".basic-edits")));
+    // $("#editBtn").click(() => panelAnimation($(".basic-edits")));
+    // $("#editBtn").click(() => panelAnimation($(".basic-edits")));
+    // $("#editBtn").click(() => panelAnimation($(".basic-edits")));
+    // $("#editBtn").click(() => panelAnimation($(".basic-edits")));
+
 
     canvas.click(uploadPhoto);
 });
